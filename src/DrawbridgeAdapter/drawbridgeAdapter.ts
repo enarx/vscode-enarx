c5e8e2bc676911987acb0030d5e1f7a7a2ed0106
Mayank Kumar <mayank22oct@gmail.com>
2022-11-07 17:39:39 +0530

    fix: case issue with enarxtomlschema and enarx toml example

--- src/DrawbridgeAdapter/drawbridgeAdapter.ts
+++ src/DrawbridgeAdapter/drawbridgeAdapter.ts
@@ -15,13 +15,6 @@ export class DrawbridgeAdapter {
             client_id: '4NuaJxkQv8EZBeJKE56R57gKJbxrTLG2',
             token_endpoint_auth_method: 'none'
         });
-        // let client = new openid.BaseClient({ client_id: '4NuaJxkQv8EZBeJKE56R57gKJbxrTLG2', token_endpoint_auth_method: 'client_secret_post'});
-        // client.issuer.metadata.authorization_endpoint = 'https://auth.profian.com/authorize';
-        // client.issuer.metadata.token_endpoint = 'https://auth.profian.com/oauth/token';
-        // client.issuer.metadata.mtls_endpoint_aliases = {
-        // 	device_authorization_endpoint: 'https://auth.profian.com/oauth/oauth/device/code',
-        // 	token_endpoint: 'https://auth.profian.com/oauth/token'
-        // };
         let a = await client.deviceAuthorization();
         let aa = await vscode.window.showInformationMessage(`Login proceedure started, continue by opening the authorization website and entering the user code [${a.user_code}]. Continue opening the authorization website and copy user code to clipboard.`,
             'Continue',
